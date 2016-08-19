fac4e3db4d0025299805ddd0c9aed51e236c4270
Jth Rdz <fernetsystem@gmail.com>
2016-08-18 23:09:58 -0500

    Control de excepciones Throws

---
+++ src/java/Models/ControlException.java
@@ -0,0 +1,20 @@
+package Models;
+
+public class ControlException extends Exception{
+
+    public ControlException() {
+    }
+
+    public ControlException(String message) {
+        super(message);
+    }
+
+    public ControlException(Throwable cause) {
+        super(cause);
+    }
+
+    public ControlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
