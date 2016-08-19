8a3c7875134a877d34b1cb46a6b0635bca60010e
Jth Rdz <fernetsystem@gmail.com>
2016-08-18 23:07:14 -0500

    creacion de paquete Beans donde clase cliente

---
+++ src/java/Beans/Cliente.java
@@ -0,0 +1,80 @@
+package Beans;
+
+public class Cliente {
+
+    private int idCliente;
+    private String nombre;
+    private String paterno;
+    private String materno;
+    private boolean sexo;
+    private String estado;
+
+    public Cliente() {
+    }
+
+    public Cliente(int idCliente, String nombre, String paterno, String materno, boolean sexo, String estado) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.paterno = paterno;
+        this.materno = materno;
+        this.sexo = sexo;
+        this.estado = estado;
+    }
+
+    public Cliente(String nombre, String paterno, String materno, boolean sexo, String estado) {        
+        this.nombre = nombre;
+        this.paterno = paterno;
+        this.materno = materno;
+        this.sexo = sexo;
+        this.estado = estado;
+    }
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaterno() {
+        return paterno;
+    }
+
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    public String getMaterno() {
+        return materno;
+    }
+
+    public void setMaterno(String materno) {
+        this.materno = materno;
+    }
+
+    public boolean isSexo() {
+        return sexo;
+    }
+
+    public void setSexo(boolean sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+}
